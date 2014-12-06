743a83c999cc02f610faa487e84b092137e64ff7
shidq <809168323@qq.com>
2014-12-06 21:10:17 +0800

    12111831

--- tree/Tree.cpp
+++ tree/Tree.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+void MyTree::setcmp(int (*f)(void *s,void *o)) {
+  comp = f;
+}
+
+struct tree* MyTree::data() {
+  return root;
+}
+
 struct tree* MyTree::tree_alloc(void* node) {
   struct tree* t=new struct tree;
   t->rt=t->lf=NULL;
@@ -12,53 +20,154 @@ struct tree* MyTree::tree_alloc(void* node) {
 void MyTree::tree_traversal(struct tree* t, void(*func)(struct tree* t)) {
   if(!t)
     return;
-   func(t);	
-   tree_traversal(t->lf, func);
-   tree_traversal(t->rt, func);
+  func(t);	
+  tree_traversal(t->lf, func);
+  tree_traversal(t->rt, func);
 }
 
-void MyTree::tree_insert(void* node) {
+int MyTree::tree_insert(void* node) {
   struct tree* t = tree_alloc(node);
   int type=ROOT;
   if(root==NULL) {
 	root=t;
 	t->type=type;
-	return;
+	return 0;
   } else {
     struct tree* tmp = root;
+	struct tree* tnode = root;
 	while(tmp) {
-      if(0 < cmp(tmp->value, node)) {
-	    tmp=root->lf;
+	  int iRet = comp(tmp->value, node);
+	  tnode=tmp;
+      if(0 < iRet) {
+	    tmp=tmp->lf;
 		type=LEFT;
-	  } else {
-	    tmp=root->rt;
+	  } else if(iRet <0) {
+	    tmp=tmp->rt;
 		type=RIGHT;
+	  } else {
+	    delete t;
+	    return -1;
 	  }
 	}
-	tmp=t;
+    if(type==RIGHT) {
+	  tnode->rt=t;
+	} else {
+	  tnode->lf=t;
+	}
 	t->type=type;
-	return;
+	return 0;
   }
-  return;
+  return 0;
   
 }
 
-void MyTree::tree_find(void* node) {
+struct tree*  MyTree::tree_find(void* node) {
   struct tree* tmp = root;
   while(tmp) {
-    int iRet = cmp(tmp->value, node);
-	if(0 < cmp(tmp->value, node)) {
-      tmp=root->lf;
-	  type=LEFT;
+    int iRet = comp(tmp->value, node);
+    if(0 < iRet) {
+	  tmp=tmp->lf;
+    } else if(iRet > 0) {
+	  tmp=tmp->rt;
     } else {
-      tmp=root->rt;
-	  type=RIGHT;
+	  break;
     }
   }
+  return tmp;
 }
 
-void MyTree::tree_insert(void* node) {
-
+int MyTree::tree_delete(void* node) {
+  struct tree* t = root;
+  struct tree* tmp=NULL;
+  
+  while(t) {
+    int iRet = comp(t->value, node);
+    if(0 < iRet) {
+	  tmp=t;
+	  t=t->lf;
+    } else if(iRet < 0) {
+	  tmp=t;
+	  t=t->rt;
+    } else {
+	  break;
+    }
+  }
+  
+  if(!t) {
+    return -1;
+  }
+  
+  if(t->type==LEFT) {
+    if (t->lf == NULL) {
+	  tmp->lf=t->rt;
+	  if(t->rt)
+	    t->rt->type=LEFT;
+	} else if (t->rt == NULL) {
+	  tmp->lf=t->lf;
+	} else {
+	  struct tree* rtmp=NULL;
+	  t->rt->type=LEFT;
+	  tmp->lf = t->rt;
+	  rtmp=t->rt->lf;
+	  t->rt->lf=t->lf;
+	  if(rtmp) {
+	    struct tree* rltmp = t->lf;
+	    while(rltmp->rt) {
+	      rltmp=rltmp->rt;
+	    }
+	    rtmp->type=RIGHT;
+	    rltmp->rt=rtmp;
+	  }
+	}
+  } else if(t->type==RIGHT){
+    if (t->lf == NULL) {
+	  tmp->rt=t->rt;
+	} else if (t->rt == NULL) {
+	  tmp->rt=t->lf;
+	  if(t->lf)
+	    t->lf->type=RIGHT;
+	} else {
+	  struct tree* ltmp=NULL;
+	  tmp->rt = t->lf;
+	  t->lf->type=RIGHT;
+	  ltmp=t->lf->rt;
+	  t->lf->rt=t->rt;
+	  if(ltmp) {
+	    struct tree* rltmp = t->rt;
+	    while(rltmp->lf) {
+	      rltmp=rltmp->lf;
+	    }
+	    ltmp->type=LEFT;
+	    rltmp->lf=ltmp;
+      }		
+	}   
+  } else {
+	if (t->lf == NULL) {
+	  root=t->rt;
+	  if(t->rt)
+	    t->rt->type=ROOT;
+	} else if (t->rt == NULL) {
+      root=t->lf;
+	  if(t->lf)
+	    t->lf->type=ROOT;
+	} else {
+      struct tree* ltmp=NULL;
+	  root = t->lf;
+	  t->lf->type=ROOT;
+	  ltmp=t->lf->rt;
+	  t->lf->rt=t->rt;
+	  if(ltmp) {
+	    struct tree* rltmp = t->rt;
+	    while(rltmp->lf) {
+	      rltmp=rltmp->lf;
+	    }
+	    ltmp->type=LEFT;
+	    rltmp->lf=ltmp;
+      }	  
+	}	
+  }
+  delete t;
+  
 }
 
 
--- tree/Tree.h
+++ tree/Tree.h
@@ -1,5 +1,6 @@
 #ifndef __TREE_H
 #define __TREE_H
+#include <stdio.h>
 
 enum Type{
   ROOT=0,
@@ -10,23 +11,28 @@ enum Type{
 
 struct tree {
   struct tree* lf;
-  struct test* rt;
+  struct tree* rt;
   int type;
   void *value;
 };
 
 class MyTree{
 public:
-  MyTree();
-  ~MyTree();
-  void* tree_init(struct tree* t);
+  MyTree():root(NULL){};
+  ~MyTree(){};
+  struct tree* tree_alloc(void* node);
+  void tree_traversal(struct tree* t, void(*func)(struct tree* t));
+  int tree_insert(void* node);
+  struct tree* tree_find(void* node);
+  int tree_delete(void* node);
+  void setcmp(int (*f)(void *s,void *o));
+  struct tree* data();
 
 private:
   struct tree* root;
   int (*comp)(void *s,void *o); 
-
-friend void tree_traversal(struct tree* t, void(*func)(struct tree* t));
-}
+  void (*deloc)(void *data);
+};
 
 
 
